db885454114eb770ca3ce872fb95d16451f081c7
Adam Melnyk <adam.melnyk@gmail.com>
2015-03-19 15:56:43 -0600

    Changing constant names

--- app/assets/javascripts/rollbar.ready.js
+++ app/assets/javascripts/rollbar.ready.js
@@ -1,9 +1,9 @@
 var _rollbarConfig = {
-      endpoint: Rollbar.js_endpoint
-      accessToken: Rollbar.access_client_token,
+      endpoint: RollbarConfig.js_endpoint
+      accessToken: RollbarConfig.access_client_token,
           captureUncaught: true,
               payload: {
-                        environment: Rollbar.rails_env
+                        environment: RollbarConfig.rails_env
                               }
 };
 
