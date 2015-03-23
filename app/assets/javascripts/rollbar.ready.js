e59db65769657a379902f88e45a276da785fa1b1
Adam Melnyk <adam.melnyk@gmail.com>
2015-03-23 15:59:28 -0600

    Bumped version number, cleaned up formating

--- app/assets/javascripts/rollbar.ready.js
+++ app/assets/javascripts/rollbar.ready.js
@@ -1,9 +1,9 @@
 var _rollbarConfig = {
-      endpoint: RollbarConfig.js_endpoint,
-      accessToken: RollbarConfig.access_client_token,
-          captureUncaught: true,
-              payload: {
-                        environment: RollbarConfig.rails_env
-                              }
+  endpoint: RollbarConfig.js_endpoint,
+  accessToken: RollbarConfig.access_client_token,
+  captureUncaught: true,
+  payload: {
+    environment: RollbarConfig.rails_env
+  }
 };
 
